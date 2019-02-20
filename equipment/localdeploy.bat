768c506ec09cc24e2250e3ae4aa33e3faeb5e3e5
BeeGrinder <4447733+BeeGrinder@users.noreply.github.com>
2019-02-20 07:48:48 -0600

    fixed item typos, added effects to force powers lvl 2

--- equipment/localdeploy.bat
+++ equipment/localdeploy.bat
@@ -1,5 +1,5 @@
 e:
 cd E:\Dropbox\Dropbox\development\FG-Git\FantasyGrounds-SW5e\equipment
 "C:\Program Files\7-Zip\7z.exe" u -tzip "SW5e Equipment.mod" common.xml definition.xml thumbnail.png
-copy /B "SW5e Equipment.mod" "C:\Users\joecy\AppData\Roaming\Fantasy Grounds\modules\"
+move /Y "SW5e Equipment.mod" "C:\Users\joecy\AppData\Roaming\Fantasy Grounds\modules\"
 
---
+++ spell-mod-src/localdeploy.bat
@@ -0,0 +1,5 @@
+e:
+cd E:\Dropbox\Dropbox\development\FG-Git\FantasyGrounds-SW5e\spell-mod-src
+"C:\Program Files\7-Zip\7z.exe" u -tzip "SW5e Spell Book.mod" common.xml definition.xml thumbnail.png
+move /Y "SW5e Spell Book.mod" "C:\Users\joecy\AppData\Roaming\Fantasy Grounds\modules\"
+
