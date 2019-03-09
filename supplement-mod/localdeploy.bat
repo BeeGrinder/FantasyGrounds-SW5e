a25790e1bc88853e789d83734c708476101e372f
BeeGrinder <4447733+BeeGrinder@users.noreply.github.com>
2019-03-08 18:43:27 -0600

    moved module sources and archived old sources

--- supplement-mod/localdeploy.bat
+++ supplement-mod/localdeploy.bat
@@ -1,5 +1,5 @@
 e:
-cd E:\Dropbox\Dropbox\development\FG-Git\FantasyGrounds-SW5e\item-spell-mod
+cd E:\Dropbox\Dropbox\development\FG-Git\FantasyGrounds-SW5e\supplement-mod
 "C:\Program Files\7-Zip\7z.exe" u -tzip "SW5e Supplement.mod" common.xml definition.xml thumbnail.png
 move /Y "SW5e Supplement.mod" "C:\Users\joecy\AppData\Roaming\Fantasy Grounds\modules\"
 
