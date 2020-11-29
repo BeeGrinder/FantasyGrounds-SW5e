157185534d0f794b359e85fc5c7ebd0f25de8a14
Joe <4447733+BeeGrinder@users.noreply.github.com>
2020-11-28 18:03:26 -0600

    Fix-NPC Spell Lookup issue, operative practice at lvl 3

--- par5e/sedclassoptions.sh
+++ par5e/sedclassoptions.sh
@@ -5,6 +5,7 @@ cat client_preedit.xml |\
  sed 's/<name type="string">Consular Tradition<\/name>/<name type="string">Consular Tradition<\/name><specializationchoice type="number">1<\/specializationchoice>/g' |\
  sed 's/<name type="string">Engineering Discipline<\/name>/<name type="string">Engineering Discipline<\/name><specializationchoice type="number">1<\/specializationchoice>/g' |\
  sed 's/<name type="string">Fighter Speciality<\/name>/<name type="string">Fighter Speciality<\/name><specializationchoice type="number">1<\/specializationchoice>/g' |\
+ sed 's/<name type="string">Operative Practice<\/name>/<name type="string">Operative Practice<\/name><specializationchoice type="number">1<\/specializationchoice>/g' |\
  sed 's/<name type="string">Guardian Form<\/name>/<name type="string">Guardian Form<\/name><specializationchoice type="number">1<\/specializationchoice>/g' |\
  sed 's/<name type="string">Monastic Order<\/name>/<name type="string">Monastic Order<\/name><specializationchoice type="number">1<\/specializationchoice>/g' |\
  sed 's/<name type="string">Academic Pursuit<\/name>/<name type="string">Academic Pursuit<\/name><specializationchoice type="number">1<\/specializationchoice>/g' |\
@@ -13,4 +14,5 @@ cat client_preedit.xml |\
  > client.xml
 /c/Program\ Files/7-Zip/7z.exe a -mx9 "SW5e Player Book.zip" client.xml definition.xml thumbnail.png images tokens
 mv "SW5e Player Book.zip" "SW5e Player Book.mod"
+cp -f "SW5e Player Book.mod" "/d/games/Smiteworks/FGUData/modules"
 mv "SW5e Player Book.mod" "/d/games/Smiteworks/FGClassic/FGCData/modules"
--- supplement-mod/localdeploy.bat
+++ supplement-mod/localdeploy.bat
@@ -1,5 +1,5 @@
 c:
 cd C:\sw5e-dev\FantasyGrounds-SW5e\supplement-mod
 "C:\Program Files\7-Zip\7z.exe" u -tzip "SW5e Supplement.mod" common.xml definition.xml thumbnail.png
-move /Y "SW5e Supplement.mod" "D:\games\Smiteworks\FGClassic\FGCData\modules\"
-
+copy /Y "SW5e Supplement.mod" "D:\games\Smiteworks\FGClassic\FGCData\modules\"
+move /Y "SW5e Supplement.mod" "D:\games\Smiteworks\FGUData\modules"
