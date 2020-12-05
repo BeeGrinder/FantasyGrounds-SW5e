6497c3b8329ff750c4a12d98dc2e16e7fe627734
Joe <4447733+BeeGrinder@users.noreply.github.com>
2020-12-05 11:37:51 -0600

    fixed extension link to correct site

--- ext-src/localdeploy.bat
+++ ext-src/localdeploy.bat
@@ -1,5 +1,5 @@
 c:
-cd C:\sw5e-dev\FantasyGrounds-SW5e\ext-src-classic
+cd C:\sw5e-dev\FantasyGrounds-SW5e\ext-src
 "C:\Program Files\7-Zip\7z.exe" u -tzip "sw5e.ext" extension.xml fonts graphics scripts strings
 copy /Y "sw5e.ext" "D:\games\Smiteworks\FGClassic\FGCData\extensions\"
 move /Y "sw5e.ext" "D:\games\Smiteworks\FGUData\extensions\"
