a334bcc2d9354a3dbae33a0228f76e642af9c69f
Joe <4447733+BeeGrinder@users.noreply.github.com>
2020-12-12 21:10:14 -0600

    all equipment updates done.  new module

--- supplement-mod/localdeploy.bat
+++ supplement-mod/localdeploy.bat
@@ -1,5 +1,5 @@
 c:
-cd C:\sw5e-dev\FantasyGrounds-SW5e\supplement-mod
+cd "C:\sw5e-dev\FantasyGrounds-SW5e\supplement-mod\SW5e Supplement"
 "C:\Program Files\7-Zip\7z.exe" u -tzip "SW5e Supplement.mod" common.xml definition.xml thumbnail.png
 copy /Y "SW5e Supplement.mod" "D:\games\Smiteworks\FGClassic\FGCData\modules\"
 move /Y "SW5e Supplement.mod" "D:\games\Smiteworks\FGUData\modules"
